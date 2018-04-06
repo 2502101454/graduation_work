0b5de559adf135eb5fcfd9cfff9ff4d621cfcf0e
wangzeng <zeng.wangz@zenjoy.net>
2018-04-06 11:28:37 +0800

    update models

--- oa_core/models.py
+++ oa_core/models.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
-class Question(models.Model):
+class Employee(models.Model):
     e_id = models.AutoField(primary_key=True)
     e_name = models.CharField(max_length=20)
     e_sex = models.IntegerField(max_length=1)
